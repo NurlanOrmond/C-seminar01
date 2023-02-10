80410121aa5f25e4e090a1227c6a7262817ae11a
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-10 13:22:22 +0300

    task 1

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine ("Input your number, please  ");
+
+int a = Convert.ToInt32(Console.ReadLine ());
+
+if (a==1){
+    Console.WriteLine ("Monday");
+}
+if (a==2){
+    Console.WriteLine ("Tuesday");
+
+}
+if (a==3){
+    Console.WriteLine ("Wednesday");
+}
+if (a==4){
+    Console.WriteLine ("Thursday");
+}
+
+if (a==5){
+    Console.WriteLine ("Friday");
+
+}
+if (a==6){
+    Console.WriteLine ("Subbath");
+}
+if (a==7){
+    Console.WriteLine ("Sunday");
+}
---
+++ task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Input two numbers to check if the first numebr is square root of the second");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());;
+
+if (a*a==b) {
+    Console.WriteLine ("That's true!");
+} else
+Console.WriteLine ("No, that's not true!");
+
