6c176947cca4429e0ff4d1d64916fb2fc28e5d4f
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-10 17:41:25 +0300

    Task_4 reamainder

---
+++ task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Input three-digit number:  " );
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<100||a>1000) {
+    Console.WriteLine ("Only three digit number, please");
+}
+else
+Console.WriteLine (a%10);
+
+Console.WriteLine ("Input three-digit number:  " );// classmate version
+int a = Convert.ToInt32(Console.ReadLine());//
+if (a/100==0||a/100>9) {//
+    Console.WriteLine ("Only three digit number, please");//
+}//
+else {//
+    int result = a%10;//
+    Console.WriteLine (a%10);//
+}//
+
