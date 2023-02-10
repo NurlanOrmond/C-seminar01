5e04ca3f67a05dc920cccd59761f3f47f0fcae22
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-10 15:51:27 +0300

    task 2

---
+++ task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = -n;
+while (i<= n) {
+    Console.Write (i);
+    Console.Write (", ");
+
+    i++;
+}
+
+
+
+
+
